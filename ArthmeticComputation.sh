d23eb40f1360658708c6de278bc4f804a95ada0e
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-07 17:01:54 +0530

    added : first operation a+b*c

--- ArthmeticComputation.sh
+++ ArthmeticComputation.sh
@@ -4,3 +4,7 @@ echo "WELCOME TO ARITHMETIC COMPUTATION AND SORTING PROGRAM"
 read -p "Enter the first input: " a
 read -p "Enter the second input: " b
 read -p "Enter the third input: " c
+
+
+operation1=$(($a+$b*$c))
+echo $operation1

ba186eb63fa0437e1228304f5a04890615646602
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-07 16:55:00 +0530

    added : input from the user

--- ArthmeticComputation.sh
+++ ArthmeticComputation.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -x
 
 echo "WELCOME TO ARITHMETIC COMPUTATION AND SORTING PROGRAM"
+read -p "Enter the first input: " a
+read -p "Enter the second input: " b
+read -p "Enter the third input: " c

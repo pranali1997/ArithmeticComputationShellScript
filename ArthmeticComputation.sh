351b54887714acbd4cf542f5349847e996a1bdae
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-07 18:49:37 +0530

    modified to check indentation

--- ArthmeticComputation.sh
+++ ArthmeticComputation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 
 declare -A resultDictionary
+
 echo "WELCOME TO ARITHMETIC COMPUTATION AND SORTING PROGRAM"
+
 read -p "Enter the first input: " a
 read -p "Enter the second input: " b
 read -p "Enter the third input: " c
@@ -11,6 +13,7 @@ operation1=$(($a+$b*$c))
 operation2=$(($a*$b+$c))
 operation3=$(($c+$a/$b))
 operation4=$(($a%$b+$c))
+
 echo "first operation" $operation1
 echo "second operation" $operation2
 echo "third operation" $operation3
@@ -29,11 +32,11 @@ done
 echo "sort in descending order"
 for((i=0; i<${#resultArray[@]}; i++))
 do
-echo ${resultArray[$i]}
+	echo ${resultArray[$i]}
 done | sort -nr
 
 echo "sort in ascending order"
 for((i=0; i<${#resultArray[@]};i++))
 do
-echo ${resultArray[$i]}
+	echo ${resultArray[$i]}
 done | sort -n

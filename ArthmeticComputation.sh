516482a4cbb45cb73d93e0e665f2a22b6390d028
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-07 17:23:22 +0530

    added : fourth operation a%b+c

--- ArthmeticComputation.sh
+++ ArthmeticComputation.sh
@@ -9,6 +9,8 @@ read -p "Enter the third input: " c
 operation1=$(($a+$b*$c))
 operation2=$(($a*$b+$c))
 operation3=$(($c+$a/$b))
+operation4=$(($a%$b+$c))
 echo "first operation" $operation1
 echo "second operation" $operation2
 echo "third operation" $operation3
+echo "fourth operation" $operation4
